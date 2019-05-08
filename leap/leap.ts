07ff53e3819743785ca8d12571176a854c773d5b
Lauren Granger <helloworld@laurengranger.com>
2019-05-08 07:06:20 -0700

    pass third leap test

--- leap/leap.ts
+++ leap/leap.ts
@@ -1,8 +1,8 @@
 function isLeapYear(year: number) {
-    if (year % 4 != 0) {
-        return false;
+    if (year % 4 === 0 && year % 100 != 0) {
+        return true;
     }
-    return true;
+    return false;
 }
 
 export default isLeapYear
