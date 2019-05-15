319c2c225d5fb73eea73958faa7894d6fd298c51
Lauren Granger <helloworld@laurengranger.com>
2019-05-14 17:52:43 -0700

    Remove if/else and use direct return.

--- leap/leap.ts
+++ leap/leap.ts
@@ -1,8 +1,5 @@
 function isLeapYear(year: number) {
-    if (year % 4 === 0 && year % 100 != 0 || year % 400 ===0) {
-        return true;
-    }
-    return false;
+    return (year % 4 === 0 && year % 100 != 0 || year % 400 ===0)
 }
 
 export default isLeapYear
